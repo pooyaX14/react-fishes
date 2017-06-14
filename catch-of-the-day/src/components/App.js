d617eee60daacf6cd8e07e759d5a63fa903f3282
Purnima Gupta <purnimaguptapccs@gmail.com>
2017-06-14 23:50:58 +0530

    create loadSamples function
    created loadSamples function and set the state of fishes that
    we imported from sample-fishes.js file.
    Created props names loadSamples and assigned this function on Inventory component.
    We add it to Inventory component so that whenever clicking on the Load Sample Fishes
    button, loadSamples function run and render the data on App component that is our
    parent component.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,20 +2,49 @@ import React from 'react';
 import Header from './Header'; 
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
-class App extends React.Component {
-    render() {
-        return(
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Sea Food Market"/>
-                </div>
-                <Order />
-                <Inventory />
-                
-            </div>
-        )
-    }
+class App extends React.Component { // parent component
+	constructor() {
+		// In order to use this keyword, one must call super()
+		super();
+
+		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
+		//get initialState
+		this.state = { // storing fish form data in state.
+			fishes:{},
+			order :{}
+		};
+	}
+	addFish(fi) {
+			// update our state
+		   const fishes = {...this.state.fishes};
+		   // add in our new fish
+		   const timestamp = Date.now();
+		   fishes[`fish-${timestamp}`] = fi;
+		   // set state
+		   this.setState({ fishes:fishes });
+	}
+
+	loadSamples() {
+		this.setState({
+			fishes:sampleFishes
+		});
+	}
+
+	render() {
+		return(
+			<div className="catch-of-the-day">
+				<div className="menu">
+					<Header tagline="Fresh Sea Food Market"/>
+				</div>
+				<Order />
+				<Inventory add={ this.addFish } loadSamples={ this.loadSamples}/>
+				
+			</div>
+		)
+	}
 }
 
 export default App;
