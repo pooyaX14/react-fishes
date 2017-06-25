d267b3fe39c89feafb58542fd0708f4687c4d916
Purnima Gupta <purnimaguptapccs@gmail.com>
2017-06-26 00:46:03 +0530

    Display order state with JSX
    calculate total price of all the fishes added to the order page
    and displayed message if fish is no longer available.

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+		constructor() {
+			super();
+			this.renderOrder = this.renderOrder.bind(this);
+		}
+		renderOrder(key){
+			const fish = this.props.fishes[key];
+			const count = this.props.order[key];
+
+			if(!fish || fish.status === 'unavailable') {
+				return <li key={key}>Sorry, {fish ? fish.name: fish } is no longer available!</li>
+			}
+
+			return (
+				<li key={key}>
+					<span>{count}lbs {fish.name}</span>
+					<span className="price">{formatPrice(count * fish.price)}</span>
+				</li>
+			)
+		}
+
+
     render() {
+    	const orderIds = Object.keys(this.props.order); // array of orders which is fish's index here
+    	// console.log(orderIds);
+    	const total = orderIds.reduce((prevTotal, key) => {
+    		const fish = this.props.fishes[key];
+    		//console.log(fish);
+    		const count = this.props.order[key];
+    		console.log(count);
+    		const isAvailable = fish && fish.status === 'available';
+	    	if(isAvailable) {
+	    		return prevTotal + (count * fish.price || 0); 
+	    	}
+	    	return prevTotal;
+    	}, 0);
         return(
-          <p>Order is defined</p>
+          <div className="order-wrap">
+          	<h2>Your Order</h2>
+          	<ul className="order">
+          		{orderIds.map(this.renderOrder)}
+          		<li className="total">
+          			<strong>Total:</strong>
+          			{formatPrice(total)}
+          		</li>
+          	</ul>
+          </div>
         )
     }
 }
