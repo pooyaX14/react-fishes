8037e20c08ca99548c33a9e377f9a6e8d7c164a7
Purnima Gupta <purnimaguptapccs@gmail.com>
2017-06-29 23:18:12 +0530

    add comment

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,6 +1,6 @@
 import Rebase from 're-base';
 
-const base = Rebase.createClass({
+const base = Rebase.createClass({ /*used rebase package to connect to firebase database*/
 	apiKey: "AIzaSyDQ3gKhdZPQJaNXn1Wn4SxeQe0F_09FAlM",
     authDomain: "catch-of-the-day-4f0f7.firebaseapp.com",
     databaseURL: "https://catch-of-the-day-4f0f7.firebaseio.com",
